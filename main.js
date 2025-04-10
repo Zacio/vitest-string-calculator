d5e5b247c3dd0bb64532d21932106dc14117654e
Micael <younsi.micael@gmail.com>
2025-04-10 15:18:31 +0200

    no négatif

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
+function separator(numbers){
+    let separatorperso = ",";
+    if(numbers.startsWith("//")){
+        separatorperso = numbers.charAt(2);
+    }
+    return separatorperso;
+}
+
 export function add(numbers){
     if (numbers.length == "") {
         return 0;
     }
-    numbers = numbers.replace(/\n/g, ",");
-    numbers = numbers.split(",").map(Number);
+
+    let separatorperso = separator(numbers);
+
+    if(numbers.startsWith("//")){
+        numbers = numbers.substring(3); 
+        console.log(numbers);
+        }
+
+    numbers = numbers.replace(/\n/g, separatorperso);
+    numbers = numbers.split(separatorperso).map(Number);
+
     let sum = 0;
+
     for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < 0) {
+            return("negatives not allowed: " + numbers[i]);
+        }
         sum += numbers[i];
     }
     return sum;
--- main.test.js
+++ main.test.js
@@ -1,7 +1,19 @@
 import { expect, test } from "vitest";
 import { add } from "./main.js";
 
-test('adds 1 + 2 to equal 6', () => {
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(`1,2`)).toBe(3);
+})
+test('adds("") to equal 0', () => {
+    expect(add(``)).toBe(0);
+})
+test('adds 1 + 2 to equal 3', () => {
     expect(add(`1
-        2,3`)).toBe(6);
-  })
+        2`)).toBe(3);
+})
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(`//;1;2`)).toBe(3);
+})
+test('negative number is not alowed', () => {
+    expect(add(`//;1;-2`)).toBe("negatives not allowed: -2");
+})
