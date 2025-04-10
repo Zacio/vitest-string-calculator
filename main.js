bfff09b2ba5df8160c940b2f724f27c502b987d7
Micael <younsi.micael@gmail.com>
2025-04-10 14:56:12 +0200

    numbers is now a string and calcul is done

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 export function add(numbers){
-    return 6
+    if (numbers.length == "") {
+        return 0;
+    }
+    numbers = numbers.split(",").map(Number);
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
 }
--- main.test.js
+++ main.test.js
@@ -2,5 +2,5 @@ import { expect, test } from "vitest";
 import { add } from "./main.js";
 
 test('adds 1 + 2 to equal 6', () => {
-    expect(add(1, 2)).toBe(6)
+    expect(add(`1,2,3`)).toBe(6);
   })
